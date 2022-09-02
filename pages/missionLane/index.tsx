80041043d23bc078e338bd4c22cfb260e36ed503
Shan Batla <shan.batla@gmail.com>
2022-09-01 20:10:45 -0400

    Finish mission lane.

--- pages/missionLane/index.tsx
+++ pages/missionLane/index.tsx
@@ -1,10 +1,24 @@
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 
 export default function Form() {
-  const username = useState<string>('')
-  const password = useState<string>('')
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const isValid = username.length > 0 && password.length >= 6
 
-  function onSubmit() {}
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log('username --', username)
+    console.log('password --', password)
+    console.log('password len --', password.length)
+  }
+
+  function onUsernameChange(e: ChangeEvent<HTMLInputElement>) {
+    setUsername(e.target.value)
+  }
+
+  function onPasswordChange(e: ChangeEvent<HTMLInputElement>) {
+    setPassword(e.target.value)
+  }
   
   return (
     <div>
@@ -16,11 +30,13 @@ export default function Form() {
         <li>Submit button, conosle.log username on submit</li>
         <li>Form validation - both inputs have values, pw has 6 or more chars</li>
       </ul>
-
       <div>
-        <form onSubmit={onSubmit}></form>
+        <form onSubmit={onSubmit}>
+          <input type="text" onChange={onUsernameChange}/>
+          <input type="password" onChange={onPasswordChange}/>
+          <input type="submit" disabled={!isValid}/>
+        </form>
       </div>
-
     </div>
   )
 }
